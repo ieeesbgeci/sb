d479b8d13fcdfb45e7fbfa5356d4c5dd49376086
salwankajas <salwankajas@gmail.com>
2023-05-08 16:24:18 +0530

    new events

--- src/components/society/society.tsx
+++ src/components/society/society.tsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 export default function Society() {
     return (
         <>
-            <div className="flex opacity-75 sm:mt-48 gap-[80px_60px] flex-col items-center flex-wrap justify-between mx-auto sm:w-[100%] sm:flex-row">
-                <div className="h-12"><Image src={"/assets/cs_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto h-full"/></div>
-                <div className="h-12"><Image src={"/assets/pes_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto h-full"/></div>
-                <div className="h-12"><Image src={"/assets/ras_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto h-full"/></div>
-                <div className="h-12"><Image src={"/assets/wie_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto h-full"/></div>
+            <div className="flex sm:mt-48 gap-[80px_60px] flex-col items-center flex-wrap justify-between mx-auto sm:w-[100%] sm:flex-row">
+                <div className="h-12"><Image src={"/assets/cs_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto h-full opacity-75 hover:opacity-100 transition-opacity duration-300"/></div>
+                <div className="h-12"><Image src={"/assets/pes_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto opacity-75 h-full hover:opacity-100 transition-opacity duration-300"/></div>
+                <div className="h-12"><Image src={"/assets/ras_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto opacity-75 h-full hover:opacity-100 transition-opacity duration-300"/></div>
+                <div className="h-12"><Image src={"/assets/wie_black.png"} alt="psoter" width="0" height="0" sizes="100vw" className="w-auto opacity-75 h-full hover:opacity-100 transition-opacity duration-300"/></div>
             </div>
         </>
     )

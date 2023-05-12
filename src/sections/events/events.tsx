4ed62036d3dbd55d4767d96d1fd383ef10eb562d
salwankajas <salwankajas@gmail.com>
2023-05-12 19:54:42 +0530

    new event

--- src/sections/events/events.tsx
+++ src/sections/events/events.tsx
@@ -6,7 +6,7 @@ export default function Events(){
             <div id="Events" className="pt-32">
             <Headings textFirst="UPCOMING" textsecond="EVENTS" xFirst={163.34} xThird={14.59} contWidth="w-[288px]"/>
             <p className="w-[min(80vw,647px)] mt-6 text-center text-ms font-extralight text-[#666] mx-auto">Come join us in these exciting events!</p>
-            <Eventcard id="AGM" heading="Annual General Meeting 2023" time="3:30 PM" text="Join us at IEEE SB GECI's Annual General Meeting 2023, where we'll embark on a journey of camaraderie and excellence. Let's forge connections with fellow volunteers and launch an unforgettable year, infusing our student branch activities with renewed vigor. Together, let's ignite our passion for innovation!" month="MAY" day="08" poster="AGm.jpeg"/>
+            <Eventcard id="CIR-Q" heading="online circuit designing competition" time="Submit before 14-05-2023" text="IEEE SB RAS  GECI is here with ' CIR-Q ',an online circuit designing competition.Bring out the inner circuit designer in you and munch on this opportunity to bag prizes worth ₹1000.Online registration will start today( 12-05-2023)" month="MAY" day="14" poster="cia.jpeg" url="https://bit.ly/3LWsnwb"/>
             </div>
         </>
     )

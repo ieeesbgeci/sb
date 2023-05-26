670b4623577e8090790f5c1e44976f51e11649c6
salwankajas <salwankajas@gmail.com>
2023-05-26 14:16:38 +0530

    new event

--- src/components/card/eventcard.tsx
+++ src/components/card/eventcard.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
 import Explore from "../button/explore"
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { MdLocationCity } from 'react-icons/md'
 import { BiTimeFive } from 'react-icons/bi'
-export default function Eventcard(props: { heading: string, time: string, text: string, day: string, month: string ,url?:string,poster:string,id:string}) {
+export default function Eventcard(props: { heading: string, time: string, text: string, day: string, month: string ,url?:string,poster:string,id:string,vanue?:string}) {
     return (
         <>
             <div className="pt-24" id={props.id}>
@@ -12,9 +13,10 @@ export default function Eventcard(props: { heading: string, time: string, text: 
                         <div className="w-[90%] h-[14rem] rounded-[26px] overflow-hidden mx-auto z-[1] min-[840px]:w-[20rem] min-[840px]:h-96"><Image src={`/assets/events/${props.poster}`} width="0" height="0" alt="join ieee" sizes="100vw" className="w-full h-full" /></div></div>
                         <div className="w-full relative bottom-[148px] flex items-center mx-auto min-[840px]:w-7/12 min-[840px]:static">
                             <div className="h-full flex flex-col items-center text-center p-[20px] pt-[158px] pb-[38px] w-full bg-[rgba(248,252,251,0.7)] rounded-[26px] relative after:absolute after:z-[-3] after:inset-[-0.12em] after:bg-[#27b7f4] after:blur-[3px] after:opacity-90 after:rounded-[26px] min-[840px]:after:rounded-[26px] min-[840px]:rounded-[26px] min-[840px]:py-[38px] min-[840px]:pr-[30px] min-[840px]:pl-[50px] min-[840px]:text-start min-[840px]:block md:mt-9 items-center min-[840px]:items-start min-[840px]:ml-[-20px]">
-                                <h1 className="font-bold text-lg mb-1 capitalize">{props.heading}</h1>
+                                <h1 className="font-bold text-lg mb-2 capitalize">{props.heading}</h1>
                                 <div><BiTimeFive className="inline-block text-[#666] mr-2" /><h3 className="font-light text-xs text-[#666] font-sans inline-block">{props.time}</h3></div>
-                                <p className="text-[12px] font-extralight text-[#666] my-6">{props.text}</p>
+                                {props.vanue?<div><MdLocationCity className="inline-block text-[#666] mr-2" /><h3 className="font-light text-xs text-[#666] font-sans inline-block">{props.vanue}</h3></div>:""}
+                                <p className="text-[12px] font-extralight text-[#666] my-4">{props.text}</p>
                                 {props.url?<Explore name="SUBMIT" icon={AiOutlineArrowRight} url={props.url} />:""}
                             </div>
                         </div>
--- src/sections/events/events.tsx
+++ src/sections/events/events.tsx
@@ -6,7 +6,7 @@ export default function Events(){
             <div id="Events" className="pt-32">
             <Headings textFirst="UPCOMING" textsecond="EVENTS" xFirst={163.34} xThird={14.59} contWidth="w-[288px]"/>
             <p className="w-[min(80vw,647px)] mt-6 text-center text-ms font-extralight text-[#666] mx-auto">Come join us in these exciting events!</p>
-            <Eventcard id="CIR-Q" heading="online circuit designing competition" time="Submit before 14-05-2023" text="IEEE SB RAS  GECI is here with ' CIR-Q ',an online circuit designing competition.Bring out the inner circuit designer in you and munch on this opportunity to bag prizes worth ₹1000.Online registration will start today( 12-05-2023)" month="MAY" day="14" poster="Cia.jpeg" url="https://bit.ly/3LWsnwb"/>
+            <Eventcard id="IR" heading="INTRODUCTION TO ROBOTICS AND ITS APPLICATIONS" time="7:30-8:30 pm" text="IEEE SBC RAS GECI is organizing an online talk session on 'INTRODUCTION TO ROBOTICS AND ITS APPLICATIONS'. Don't  miss this opportunity. Ignite your passion in robotics and join our class" month="MAY" day="29" poster="IR.jpeg" url="https://forms.gle/8YtnYvzyEyq4822t7" vanue="Google meet"/>
             </div>
         </>
     )

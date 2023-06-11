08bade193f2127eda94c1a1795d69d7f1eba3034
VISHNU V <95264813+Vishnu-V8666@users.noreply.github.com>
2023-06-11 10:12:00 +0530

    Update events.tsx

--- src/sections/events/events.tsx
+++ src/sections/events/events.tsx
@@ -6,7 +6,7 @@ export default function Events(){
             <div id="Events" className="pt-32">
             <Headings textFirst="UPCOMING" textsecond="EVENTS" xFirst={163.34} xThird={14.59} contWidth="w-[288px]"/>
             <p className="w-[min(80vw,647px)] mt-6 text-center text-ms font-extralight text-[#666] mx-auto">Come join us in these exciting events!</p>
-            <Eventcard id="bee" heading="INTEGRATION BEE" time="04:30 pm" text="IEEE SB GECI is organizing a competitive event INTEGRATION BEE based on integrals
+            <Eventcard id="integration_bee" heading="INTEGRATION BEE" time="04:30 pm" text="IEEE SB GECI is organizing a competitive event INTEGRATION BEE based on integrals
 
 Integration Bee is  a platform for students to showcase their creativity, problem-solving skills, and love for mathematics" month="JUN" day="13" poster="bee.jpeg" url="https://forms.gle/YXktEkEaPbxrgd6i9" vanue="CS 401 Classroom"/>
             </div>

b72ff4677bb50c6a258d8088d383809a3fe18490
salwankajas <salwankajas@gmail.com>
2023-06-11 10:21:34 +0530

    new event

--- src/sections/events/events.tsx
+++ src/sections/events/events.tsx
@@ -6,7 +6,7 @@ export default function Events(){
             <div id="Events" className="pt-32">
             <Headings textFirst="UPCOMING" textsecond="EVENTS" xFirst={163.34} xThird={14.59} contWidth="w-[288px]"/>
             <p className="w-[min(80vw,647px)] mt-6 text-center text-ms font-extralight text-[#666] mx-auto">Come join us in these exciting events!</p>
-            <Eventcard id="IR" heading="INTRODUCTION TO ROBOTICS AND ITS APPLICATIONS" time="7:30-8:30 pm" text="IEEE SBC RAS GECI is organizing an online talk session on 'INTRODUCTION TO ROBOTICS AND ITS APPLICATIONS'. Don't  miss this opportunity. Ignite your passion in robotics and join our class" month="MAY" day="29" poster="IR.jpeg" url="https://forms.gle/8YtnYvzyEyq4822t7" vanue="Google meet"/>
+            <Eventcard id="integralbee" heading="INTEGRATION BEE" time="4:30 pm" text={`IEEE SB GECI is organizing a competitive event "INTEGRATION BEE" based on ✨️integrals. Integration Bee is  a platform for students to showcase their creativity, problem-solving skills, and love for mathematics. Win cash prize of 1.5k🎁`} month="JUN" day="13" poster="bee.jpeg" url="https://forms.gle/YXktEkEaPbxrgd6i9" vanue="CS 401 Classroom"/>
             </div>
         </>
     )
